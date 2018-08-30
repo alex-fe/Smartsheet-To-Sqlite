db3d69e43371fb9b0311843a864eeeab590a2a45
Alex Feldman <alexfeldman93@gmail.com>
2018-08-30 15:50:47 -0700

    Move to a OOP heirarchy

--- smartsheet_app.py
+++ smartsheet_app.py
@@ -1,21 +1,64 @@
 import argparse
-import operator
 import os
 import smartsheet
 import sqlite3
 import yaml
 
 
-COLUMN_TYPES = {
-    'ABSTRACT_DATETIME': 'NUMERIC',
-    'CHECKBOX': 'INTEGER',
-    'CONTACT_LIST': 'TEXT',
-    'DATE': 'NUMERIC',
-    'DURATION': 'TEXT',
-    'PICKLIST': 'TEXT',
-    'PREDECESSOR': 'TEXT',
-    'TEXT_NUMBER': 'TEXT'
-}
+class Column(object):
+    def __init__(self, column, column_mapping):
+        self.title = column.title
+        self.type = column.type
+        self.data = ''
+        self.ss_name = column_mapping['ss_col_name']
+        self.db_name = column_mapping['db_col_name']
+
+
+class Sheet(object):
+    def __init__(self, config):
+        self.columns = []
+        self.columns_list = [mp['ss_col_name'] for mp in config['mappings']]
+        self.db_file = config['db_file']
+        self.mappings = config['mappings']
+        self.name = config['sheet_name']
+        self.table = self.name.replace(' ', '_')
+        self.token = config['access_token']
+
+    @property
+    def column_strs(self):
+        return ', '.join(col.db_name for col in self.columns)
+
+    def get_columns(self):
+        for column_mapping in self.mappings:
+            try:
+                column = next(
+                    col for col in self.sheet.columns
+                    if column_mapping['ss_col_name'] == col.title
+                )
+            except StopIteration:
+                pass
+            else:
+                self.columns.append(Column(column, column_mapping))
+
+    def pull(self):
+        """Find and parse specified sheet via smartsheet api.
+        Args:
+            config (dict): Parsed .yml file.
+        Returns:
+            Sheet object based on name given in .yml attribute 'sheet_name'.
+        """
+        ss_client = smartsheet.Smartsheet(self.token)
+        ss_client.errors_as_exceptions(True)
+        sheets = ss_client.Sheets.list_sheets(include_all=True).data
+        try:
+            self.id = next(
+                sheet.id for sheet in sheets if sheet.name == self.name
+            )
+        except StopIteration:
+            raise SystemExit('Sheet {} was not found'.format(self.name))
+        else:
+            self.sheet = ss_client.Sheets.get_sheet(self.id)
+            self.get_columns()
 
 
 def process_yml(path):
@@ -34,74 +77,19 @@ def process_yml(path):
         raise RuntimeError('Incorrect path: {}'.format(path))
     with open(path, 'r') as f:
         config = yaml.load(f)
-    config['mappings'].sort(key=operator.itemgetter('ss_col_name'))
-    return config
+    return Sheet(config)
 
 
-def pull_smartsheet(config):
-    """Find and parse specified sheet via smartsheet api.
-    Args:
-        config (dict): Parsed .yml file.
-    Returns:
-        Sheet object based on name given in .yml attribute 'sheet_name'.
-    """
-    ss_client = smartsheet.Smartsheet(config['access_token'])
-    ss_client.errors_as_exceptions(True)
-    sheets = ss_client.Sheets.list_sheets(include_all=True).data
-    # TODO: update so if more than one sheet is selected do something
-    try:
-        sheet_id = next(
-            sheet.id for sheet in sheets if sheet.name == config['sheet_name']
-        )
-    except StopIteration:
-        raise SystemExit('Sheet {} was not found'.format(config['sheet_name']))
-    else:
-        return ss_client.Sheets.get_sheet(sheet_id)
-
-
-def filter_cols(columns, mappings):
-    """Filter the needed columns from the total sheet's columns.
-    Args:
-        columns (list): The sheets total list of columns.
-        mappings(list): The mappings gleaned from config.yml
-    Returns:
-        Filtered list of columns
-    """
-    return (
-        col for col in columns
-        if col.title in [mp['ss_col_name'] for mp in mappings]
-    )
-
-
-def create_sql_command(sheet, config):
-    """Create sqlite command with table name and columns.
-    Args:
-        sheet (Sheet): Sheet object based on name given in .yml attribute
-            'sheet_name'.
-        config (dict): Parsed .yml file.
-    Returns:
-            String formatted message to create desired table.
-    """
-    sql_columns = []
-    columns = filter_cols(sheet.columns, config['mappings'])
-    for m, col in zip(config['mappings'], columns):
-        col = '{} {}'.format(m['db_col_name'], COLUMN_TYPES[col.type])
-        sql_columns.append(col)
-    table = sheet.name.replace(' ', '_')
-    columns = ', '.join(sql_columns)
-    return 'CREATE TABLE {} ({})'.format(table, columns)
-
-
-def to_sql(sheet, config):
+def to_sql(sheet):
     """Produce sqlite database from smartsheet columns.
     Args:
         sheet (Sheet): Sheet object based on name given in .yml attribute
             'sheet_name'.
         config (dict): Parsed .yml file.
     """
-    conn = sqlite3.connect(config['db_file'])
+    conn = sqlite3.connect(sheet.db_file)
     conn = conn.cursor()
-    conn.execute(create_sql_command(sheet, config))
+    conn.execute('CREATE TABLE {} ({})'.format(sheet.table, sheet.column_strs))
     conn.commit()
     conn.close()
 
@@ -115,8 +103,6 @@ if __name__ == '__main__':
         help='Path from current directory to config.yml'
     )
     args = parser.parse_args()
-    config = process_yml(args.config)
-    # sheet = pull_smartsheet(config)
-    # to_sql(sheet, config)
-    from pprint import pprint
-    pprint(config)
+    sheet = process_yml(args.config)
+    sheet.pull()
+    to_sql(sheet)
